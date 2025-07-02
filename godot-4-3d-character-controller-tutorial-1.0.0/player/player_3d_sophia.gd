e0a0abf0a1462cb99ca059ff366e55b635311073
Nicolas Agustín Perez <nicolasperez95.a@gmail.com>
2025-07-02 20:34:38 -0300

    finish 3d camera course

--- godot-4-3d-character-controller-tutorial-1.0.0/player/player_3d_sophia.gd
+++ godot-4-3d-character-controller-tutorial-1.0.0/player/player_3d_sophia.gd
@@ -8,8 +8,14 @@ extends CharacterBody3D
 @export_category("Movement")
 @export var move_speed := 8.0
 @export var acceleration := 20.0
+@export var rotation_speed := 12.0
+@export var jump_impulse := 12.0
 
 var _camera_input_direction := Vector2.ZERO
+var _last_movement_direction := Vector3.BACK
+var _gravity := -30.0
+
+@onready var _skin = %SophiaSkin
 @onready var _camera_pivot : Node3D = %CameraPivot
 @onready var _camera_3d: Camera3D = %Camera3D
 
@@ -27,11 +33,48 @@ func _input(event: InputEvent) -> void:
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE		
 
 func _physics_process(delta: float) -> void:
+	## - Camera Movement -
 	_camera_pivot.rotation.x += _camera_input_direction.y * delta
 	## Limits the camera movent (to not fully rotate)
 	_camera_pivot.rotation.x = clamp(_camera_pivot.rotation.x, tilt_lower_limit, tilt_upper_limit)
 	
 	_camera_pivot.rotation.y -= _camera_input_direction.x * delta
-	
 	## Prevents camera to keep move if mouse is stoped
 	_camera_input_direction = Vector2.ZERO
+	
+	## - Player Movement -
+	var raw_input := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	var forward := _camera_3d.global_basis.z
+	var right := _camera_3d.global_basis.x
+	
+	var move_direction := forward * raw_input.y + right * raw_input.x
+	## prevents to move in y exes due to camera rotation
+	move_direction = move_direction.normalized()
+	var velocity_y = velocity.y
+	move_direction.y = 0.0
+	
+	velocity = velocity.move_toward(move_direction * move_speed, delta * acceleration)
+	velocity.y = velocity_y + _gravity * delta
+	move_and_slide()
+
+	var is_starting_jump := Input.is_action_just_pressed("jump") and is_on_floor()
+	if is_starting_jump:
+		velocity.y += jump_impulse
+	
+	## - Region Rotation -
+	if (move_direction.length() > 0.2):
+		_last_movement_direction = move_direction
+	var target_rotation := Vector3.BACK.signed_angle_to(_last_movement_direction, Vector3.UP)
+	_skin.global_rotation.y = lerp_angle(_skin.global_rotation.y, target_rotation, delta * rotation_speed)	
+
+	## - Region Animation -
+	var ground_speed := velocity.length()
+	if is_starting_jump:
+		_skin.jump()
+	elif not is_on_floor() and velocity.y < 0:
+		_skin.fall()
+	elif is_on_floor():
+		if ground_speed > 0.2:
+			_skin.move()
+		else:
+			_skin.idle()
